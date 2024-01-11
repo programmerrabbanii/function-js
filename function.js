c818c6b952a5f27bfe8051e04063ad84357cf8d5
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-01-11 15:00:38 +0600

    function added

--- function.js
+++ function.js
@@ -1,7 +1,29 @@
 // function declarition
-function startFan(){
-    console.log('stand up')
-    console.log('walk  towards the switch')
-    console.log('press the switch')
+// function startFan(){
+//     console.log('stand up')
+//     console.log('walk  towards the switch')
+//     console.log('press the switch')
+// }
+// startFan()
+
+// function singara(money){
+//     console.log('singara daw')
+//     console.log(money);
+// }
+// singara(250)
+
+// function bringSigara(money){
+//     console.log('taka disen', money)
+//     console.log('ei nen sigara')
+
+// }
+// var givenTk=140;
+// bringSigara(givenTk)
+
+function sum(a,b,c,d,f){
+    console.log(a,c,d,f)
+    var sum=a+b+c+d+f;
+    console.log(sum)
+
 }
-startFan()
+sum(44,55,77,88,11)
