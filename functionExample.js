5a6e322efb329048676a0ed332243082cfa3409f
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-01-15 11:24:48 +0600

    added the assing

---
+++ functionExample.js
@@ -0,0 +1,35 @@
+function add(sum1,sum2){
+    var sum=sum1+sum2;
+    return sum
+}
+ var tatalCost= add(20,10)
+//  console.log(tatalCost)
+
+function bringSigara(money){
+    var sigaraPrice=10;
+    var quantity=money / sigaraPrice;
+    return quantity;
+    
+    
+
+}
+var givenPrices=300;
+var getSigara= bringSigara(givenPrices)
+// console.log(getSigara)
+
+function bringSigara(money){
+    var sigaraPrice=10;
+    var quantity=money /sigaraPrice;
+    return quantity
+    
+}
+var totalSigara=bringSigara(600)
+// console.log(totalSigara)
+
+function getAssignmentMarks(assig1,assig2,assig3){
+    var totalMarks=assig1+assig2+assig3;
+    var avarage=totalMarks /3;
+    return avarage;
+}
+var total=getAssignmentMarks(60,58,59)
+// console.log(total)
---
+++ returen.js
@@ -0,0 +1,36 @@
+
+// function bringSigara(money){
+//     console.log('ei nen sigara')
+//     console.log('ei nan tk', money)
+// }
+// bringSigara(66);
+
+// function add(num1, num2){
+//     console.log(num1,num2)
+//     var sum=num1 +num2;
+//     console.log(sum)
+// }
+// add(45,15)
+
+// function bringSigara(money){
+//     var sigaraPrices=10;
+//     var quantity=money / sigaraPrices;
+//     return quantity;
+
+// }
+// var givenMoney=400;
+// var totalCost=bringSigara(givenMoney)
+// console.log(totalCost)
+
+
+
+// function burger(money){
+//     var echBueger=20;
+//     var pepule=5;
+//     var division=echBueger /pepule;
+//     return division;
+// }
+// var total= burger(20)
+// console.log(total)
+
+
